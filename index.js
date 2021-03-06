cd3c3f7a8c46923eabaded6b86e00eabe31dd815
Taral Panchal <taral.facile@outlook.com>
2021-03-06 15:15:59 +0530

    Comparison operator

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
  let x= 10;
  let y= 2;
 
- console.log(x += 5);
- console.log(x -= 5);
- console.log(x *= 5);
- console.log(x /= 5);
-
-//  x *= 5;
-//  x /= 5;
+console.log(x > 10); //gt
+console.log(x >= 10);  //gt equal
+console.log(x < 10);  // lt
+console.log(x <= 10);  //lt equal
+console.log(x === 10); // equal 
+console.log(x !== 10);   //not-equal

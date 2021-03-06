e6889678a8dfe99459ad9f83c82aa6b548dc20c7
Taral Panchal <taral.facile@outlook.com>
2021-03-06 15:02:39 +0530

    Airethmetic Operator

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function squre(number){  
-    return number * number;
-}
-console.log(squre(2)); 
+ let x= 10;
+ let y= 2;
+
+ console.log(x + y);
+ console.log(x - y);
+ console.log(x * y);
+ console.log(x / y);
+ console.log(x % y);
+ console.log(x ** y);
+ //Increment 
+ console.log(x++);
+ console.log(x);
+//  Descrement
+ console.log(--y);

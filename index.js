d5552dc25b717fd245dba8a6bca355cae81cf82d
Taral Panchal <taral.facile@outlook.com>
2021-03-06 15:10:03 +0530

    assignment operator

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
  let x= 10;
  let y= 2;
 
- console.log(x + y);
- console.log(x - y);
- console.log(x * y);
- console.log(x / y);
- console.log(x % y);
- console.log(x ** y);
- //Increment 
- console.log(x++);
- console.log(x);
-//  Descrement
- console.log(--y);
+ console.log(x += 5);
+ console.log(x -= 5);
+ console.log(x *= 5);
+ console.log(x /= 5);
+
+//  x *= 5;
+//  x /= 5;

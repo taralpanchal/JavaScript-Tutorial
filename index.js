2a00623f3f2b7d2504ec4f097182eedff4f2bc8c
Taral Panchal <taral.facile@outlook.com>
2021-03-06 18:02:18 +0530

    if..else  coditional statement

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
- //Read Write Execute
- //000000100
- //000000010
- //000000001  
- const readPermission = 4;
- const writePermission = 2;
- const executrPermission = 1;
+let hour = 19;
+if(hour >= 6 && hour < 12)
+    console.log('Good Morning');
 
- let myPermission = 0;
- myPermission = myPermission | readPermission | writePermission;
+else if(hour >= 12 && hour < 18)
+    console.log('Good Afternoon');
 
- let message = (myPermission & readPermission) ? 'yes' : 'no';
- console.log(message);
+else
+    console.log('Good Evening');

2890731bdade88b0bb5e5d25e2a541ad2c5f9585
Taral Panchal <taral.facile@outlook.com>
2021-03-08 15:02:22 +0530

    Pattern ex

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
+showStars(5);
 
-//  1-59 F
-// 60-69 D
-//  70-79- C
-// 80-89 -B
-// 90-100 A
-const marks = [70,70,90]
- console.log(calculateGrade(marks));
-
-function calculateGrade(marks){
-    const average = calculateAverage(marks);
-    if(average < 60) return 'F';
-    if(average < 70) return 'D';
-    if(average < 80) return 'C';
-    if(average < 90) return 'B';
-     return 'A';
-}
-function calculateAverage(array){
-    let sum = 0;
-    for (let value of array)
-        sum += value;
-    return sum / array.length;
+function showStars(rows){
+    for(let i=0; i <= rows ; i++){
+        let pattern = '';
+        for(let j=0; j <= i; j++)
+            pattern += ' *';
+            console.log(pattern);
+    }
 }

00b1d6758d796b4d511bc55163019fa17987c5d5
Taral Panchal <taral.facile@outlook.com>
2021-03-08 14:33:32 +0530

    String properties Ex

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
- const array = [0, null, undefined, '', 2, 3];
-console.log(countTruthy(array));
+const movie = {
+    tital : 'a',
+    releaseYear : 2018,
+    rating : 4.5,
+    director:'b'
 
-function countTruthy(array) {
-    let count = 0;
-    for(let value of array)
-    if (value)
-        count++;
-        return count; 
+}
+showProperties(movie);
+function showProperties(obj){
+    for(let key in obj){
+        if(typeof obj[key] === 'number') //string
+            console.log(key, obj[key]);
+    } 
 }

3ead26d115ed512f05a38e46a6d3fb5901142f83
Taral Panchal <taral.facile@outlook.com>
2021-03-08 18:39:32 +0530

    Clone object

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const circle = {
         console.log('draw');
     }
 };
-// for (let key in circle)
-//     console .log(key,circle[key]);
+// const another = {};
+// for(let key in circle)
+//     another[key] = circle[key];
 
-for(let key of Object.keys(circle))
-    console.log(key);
-    
-for(let entry of Object.entries(circle))
-    console.log(entry);
+const another = Object.assign({}, circle);
+console.log(another);
+
+const anopther = { ...circle};

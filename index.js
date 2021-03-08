a5b5c04be10c0dbd47ba1d76544f16224d78ee85
Taral Panchal <taral.facile@outlook.com>
2021-03-08 13:52:53 +0530

    ODD EVEN numbe Ex

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-//Speed Limit = 70
-// 5 -> 1 point
-//Math.floor(1.3)
-//more then 12 ponts -> Suspended
-checkSpeed(70);
-
-function checkSpeed(speed){
-    const speedLimit = 60;
-    const kmPerPoint = 5;
-
-    if(speed < speedLimit + kmPerPoint)
-        console.log('Ok');
-    else{
-        const points = Math.floor((speed - speedLimit) / kmPerPoint);
-        if(points > 12)
-            console.log('License suspended');
-        else
-            console.log('Ponits',points);
+showNumbers(10);
+function showNumbers(limit){
+    for(let i=0 ; i<=limit ; i++){
+        if(i % 2 == 0)
+            console.log(i,'EVEN');
+        else   
+            console.log(i,'ODD'); 
     }
- }
+}

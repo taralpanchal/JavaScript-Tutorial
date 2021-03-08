2336e2a6b3b6489ebe7af5ebc7cbf5f41bebd141
Taral Panchal <taral.facile@outlook.com>
2021-03-08 19:24:24 +0530

    Ex-1 Address object

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-//  Sting Primitive 
-// const message = 'hii!' + 
-// 'how are you?';
-
-// const another = `Thi is my 
-// fist message`; 
-const name = 'John';
-const massge = 'Hi ' + name + ',\n';
-
-const another = 
-`Hi ${name} ${2+3},
-
-Thank you for joining my mailing list
-
-Ragards,
-Mosh
-`;
+ let address = {
+    street: 'Cha',
+    city : 'Ahme',
+    zipcode : 123456
+ };
+ function showAddress(address){
+     for(let key in address)
+     console.log(key ,address[key]);
+ }
+ showAddress(address);

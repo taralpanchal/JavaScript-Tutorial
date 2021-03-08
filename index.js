65a9a17d11ecec20b285829f150e3067eec5c498
Taral Panchal <taral.facile@outlook.com>
2021-03-08 12:15:38 +0530

    for loop, while loop, do while loop, infinite loop, for..in loop, for..of loop

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-let role = 'moderator';
-switch (role){
-    case 'guest':
-        console.log('Guest User');
-        break;
+For Loop
+for(let i = 5; i >= 1 ; i--){
+   if(i % 2 !== 0) console.log(i);
+}  
 
-    case 'moderator':
-        console.log('Moderator user');
-        break;
-
-    default:
-        console.log('Unknown User');
-        break;
+While loop
+let i = 0;
+while(i<=5){
+    if(i % 2 !== 0) console.log(i);
+    i++;
 }
 
-// if(role==='guest') console.log('guest');
-// else if(role==='moderator') console.log('modeartor');
-// else(role==='unknown user') console.log('unknow user');
+Do-While loop  
+let i = 1;
+do{
+    if(i % 2 !== 0) console.log(i);
+    i++; 
+}while(i <= 5);
+
+infinite loop
+let i = 1;
+while(i <= 5){
+    console.log(i);
+  i++;
+}
+
+while(true){
+
+}
+
+let x=0;
+do{
+    
+}while(x<5);
+
+for(let i = 0; i < 10);
+
+For In Loop
+const person = {
+    name:"Taral",
+    age:21
+}
+for(let key in person)
+    console.log(key, person[key]);
+
+const colors=['red','blue','green'];
+for(let i in colors)
+    console.log(i,colors[i]);
+
+//For..Of
+ const colors=['red','blue','green'];
+ for(let color of colors)
+    console.log(color);
+
+// break and Continue
+let i = 0;
+while(i<=10){
+    // if(i === 5) break;
+    if(i % 2 == 0)
+        console.log(i);
+        i++;
+        continue;
+};

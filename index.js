56d65195f2eed3214d4f95045cd8a32b4db8fd07
Taral Panchal <taral.facile@outlook.com>
2021-03-08 13:20:53 +0530

    FezzBuzz Exercise

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+//Divisibale by 3 => Fizz
+//Divisibale by 5 => Buzz
+// Divisibale by both 3 & 5 => FizzBuzz
+//Not Divisibale by 3 & 5 => input
+//Not A Number => 'Not Number
+const output = fizzBuzz('Mosh');
+console.log(output);
 
-console.log(isLandscape(300,400));
-function isLandscape(width, height){
-    return (width > height);
-};
+function fizzBuzz(input){
+    if(typeof input !==  'number')
+        return NaN;
+
+    if(input % 3 === 0 && input % 5 ===  0)
+        return 'FizzBuzz';
+
+    else if(input % 5 ===  0)
+        return 'Buzz';
+
+    else if(input % 3 === 0)
+        return 'Fizz';
+     
+    return input;       
+}

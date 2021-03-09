e6bedcadc586cb97deb17d3ce87e3b5dff6586f3
Taral Panchal <taral.facile@outlook.com>
2021-03-09 12:02:15 +0530

    Blog post Ex

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
- function Address(street,city,zipCode){
-    this.street = street;
-    this.city = city;
-    this.zipCode = city;
- }
- let add1 = new Address('asv','asdf',1233);
- let add2 = new Address('asv','asdf',1233);
- let add3 = add1;
-
- console.log(areEqual(add1,add2));
-console.log(areSame(add1,add2));  
-console.log(areSame(add1,add3)); 
-
-function areEqual(add1,add2){
-  return add1.street === add2.street && 
-    add1.city === add2.city  && 
-    add1.zipCode === add2.zipCode;
-}
-
-
-function areSame(add1,add2){
-    return add1 === add2;    
-}
+//  function Address(title,body,author,views){
+//     this.title = title;
+//     this.body = body;
+//     this.author = author;
+//     this.views = views;
+//  }
+ const blogPost = {
+    title : 'abc',
+    body : 'Blog Post',
+    author : 'AK',
+    views : 12,
+    commenets : [
+        {author :'abc' , body : 'Bolg Post'},
+        {author :'asd' , body : 'Bolg'},
+    ],
+    isLive : true
+ };
+console.log(blogPost);

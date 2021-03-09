e9d9e5e28419d73ca68647f1b5a2cbafd1b1e2d3
Taral Panchal <taral.facile@outlook.com>
2021-03-09 14:49:22 +0530

    Sort array

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const numbers= [1,2,3];
-const joined = numbers.join(',');
-console.log(joined);
+// const numbers= [3,2,1,5,8,6];
+// console.log(numbers.sort());
 
-const message = 'This is muy first message';
-const parts = message.split(' ');
-console.log(parts);
+// console.log(numbers.reverse());
 
-const combined = parts.join('-');
-console.log(combined);
+
+const cource = [
+    { id:1, name:'Node.js'},
+    { id:2, name:'javascript'},
+];
+cource.sort(function(a,b){
+    //a < b=-1
+    //a > b=1
+    //a===b =0
+    const nameA = a.name.toLocaleLowerCase();
+    const nameB = b.name.toLocaleLowerCase();
+
+    if(nameA < nameB) return -1;
+    if(nameA > nameB) return 1;
+    return 0;
+});
+console.log(cource);

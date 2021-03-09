16964eccf8bad400015c674496570aab27e94ffb
Taral Panchal <taral.facile@outlook.com>
2021-03-09 12:08:15 +0530

    constructor in blog post Ex

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-//  function Address(title,body,author,views){
-//     this.title = title;
-//     this.body = body;
-//     this.author = author;
-//     this.views = views;
-//  }
- const blogPost = {
+const blogPost = {
     title : 'abc',
     body : 'Blog Post',
     author : 'AK',
     views : 12,
-    commenets : [
+    comments : [
         {author :'abc' , body : 'Bolg Post'},
         {author :'asd' , body : 'Bolg'},
     ],
     isLive : true
  };
-console.log(blogPost);
+
+function Post(title,body,author){
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 12;
+    this.comments = [];
+    this.isLive = false;
+ }
+ const post1 = new Post('abc','asd','aasd');
+ console.log(post1);

822073dbbf355b81218df5f7543f084843567242
Taral Panchal <taral.facile@outlook.com>
2021-03-09 11:43:37 +0530

    Object Equality Ex

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-//  let address = {
-//     street: 'Cha',
-//     city : 'Ahme',
-//     zipcode : 123456
-//  };
-
-function showAddress(street,city,zipCode){
-     return{
-         street,
-         city,
-         zipCode,
-         
-         };
-     }
-
- let add1 = new showAddress('cha','ahme',123456);
- console.log(add1);
-
  function Address(street,city,zipCode){
     this.street = street;
     this.city = city;
     this.zipCode = city;
  }
- let add2 = new showAddress('asv','asdf',1233);
- console.log(add2);
+ let add1 = new Address('asv','asdf',1233);
+ let add2 = new Address('asv','asdf',1233);
+ let add3 = add1;
+
+ console.log(areEqual(add1,add2));
+console.log(areSame(add1,add2));  
+console.log(areSame(add1,add3)); 
+
+function areEqual(add1,add2){
+  return add1.street === add2.street && 
+    add1.city === add2.city  && 
+    add1.zipCode === add2.zipCode;
+}
+
+
+function areSame(add1,add2){
+    return add1 === add2;    
+}

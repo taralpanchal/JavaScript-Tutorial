74c93355242246720e46945fc60502858da69b7e
Taral Panchal <taral.facile@outlook.com>
2021-03-09 14:14:11 +0530

    Spread operator

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let first = [{ id: 1 }];
 let second = [5, 6, 7, 8];
-const combined = first.concat(second);
-first[0].id = 5;    
-const slice = combined.slice(0,3);
-
+// const combined = first.concat(second);
+ const combined = [...first,'a',...second,'b'];
+// const slice = combined.slice(0,3);
+const copy = [...combined];
 console.log(combined);
-console.log(slice); 
+console.log(copy); 

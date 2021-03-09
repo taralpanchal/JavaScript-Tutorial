8ffb2a90572f4f66225c57c2caf5befc9c8f0797
Taral Panchal <taral.facile@outlook.com>
2021-03-09 16:42:54 +0530

    Function declaration

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const numbers= [3,2,-1,5,8,6];
-// let sum = 0;
-// for(let n of numbers)
-// sum += n;
-const sum = numbers.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-},0);
-console.log(sum);
- 
+   //function declaration
+   function walk(){
+       console.log('walk');
+   } 
+
+   //Anonymous Function Experassion
+   let run = function(){
+        console.log('run');
+   };
+   let move = run;
+   run();
+   move();

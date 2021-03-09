ed69ab30f80ffd1fee31eb88450a00131e6312a4
Taral Panchal <taral.facile@outlook.com>
2021-03-09 11:24:12 +0530

    Factori fun & constructor fun Ex

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
- let address = {
-    street: 'Cha',
-    city : 'Ahme',
-    zipcode : 123456
- };
- function showAddress(address){
-     for(let key in address)
-     console.log(key ,address[key]);
+//  let address = {
+//     street: 'Cha',
+//     city : 'Ahme',
+//     zipcode : 123456
+//  };
+
+function showAddress(street,city,zipCode){
+     return{
+         street,
+         city,
+         zipCode,
+         
+         };
+     }
+
+ let add1 = new showAddress('cha','ahme',123456);
+ console.log(add1);
+
+ function Address(street,city,zipCode){
+    this.street = street;
+    this.city = city;
+    this.zipCode = city;
  }
- showAddress(address);
+ let add2 = new showAddress('asv','asdf',1233);
+ console.log(add2);

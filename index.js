b492449ff418917b1d5208434fba567aec6f46ad
Taral Panchal <taral.facile@outlook.com>
2021-03-09 18:30:31 +0530

    try catch block

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-function interest(principal,rate=3.5,year=4){
-    return principal * rate / 100 * year;
-}
-console.log(interest(15000));
+  const person = {
+      firstName : 'Mosh',
+      lastName : 'Hamedani',
+      
+      set fullName(value){
+        if(typeof value !==  'string')
+            throw new Error('Value is not a string');
+       
+        if(parts.length !== 2)
+            throw new Error('Enter a first and last name');
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+      }
+  };
+  try{
+    person.fullName = 123;
+  }
+  catch (e) {
+      alert(e);
+  }
+  console.log(person);

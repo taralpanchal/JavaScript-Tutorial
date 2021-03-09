5ca1df2ce4c3967fbc32d7352cb7423f850cd148
Taral Panchal <taral.facile@outlook.com>
2021-03-09 18:43:29 +0530

    local & global variable

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-  const person = {
-      firstName : 'Mosh',
-      lastName : 'Hamedani',
-      
-      set fullName(value){
-        if(typeof value !==  'string')
-            throw new Error('Value is not a string');
-       
-        if(parts.length !== 2)
-            throw new Error('Enter a first and last name');
-        this.firstName = parts[0];
-        this.lastName = parts[1];
-      }
-  };
-  try{
-    person.fullName = 123;
-  }
-  catch (e) {
-      alert(e);
-  }
-  console.log(person);
+const color = 'red';
+
+function start(){
+    const message = 'hi';
+    const color = 'blue';
+    console.log(color);
+}
+function stop(){
+    const message = 'bye';
+}
+start();
+stop();

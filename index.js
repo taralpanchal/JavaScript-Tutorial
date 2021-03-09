fa6eb74dc8f867029b2a6b7dad1212558c2cbe55
Taral Panchal <taral.facile@outlook.com>
2021-03-09 16:16:11 +0530

    Map

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const numbers= [3,2,-1,5,8,6];
 
-// const atLeastOnepositive = numbers.some(function(value){
-//     return value >= 0;
-// });
-// console.log(atLeastOnepositive);
+// const filtered = numbers.filter(n => n >= 0);
 
-const every = numbers.every(function(value){
-    return value>=0;
-});
-console.log(every);
+// const item = filtered.map( n => '<li>' + n + '<li>' );
+
+// const html = '<ul>' + item.join(' ')+ '<ul> ';
+const items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({ value : n }))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value);
+
+console.log(items);  

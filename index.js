21d09b790d4db9bbb84898cec3e3b86db91237b9
Taral Panchal <taral.facile@outlook.com>
2021-03-10 15:01:22 +0530

    Map metod

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// method -> obj
-//function -> global (window , global)
-// const video = {
-//     title : 'a',
-//     play(){
-//         console.log(this);
-//     }
-// };
-// function Video(title) {
-//     this.title = title;
-//     console.log(this);
-// }
-// const v = new Video('b'); // {}.
+const numbers = [1,2,3,4,5];
 
-const video = {
-    title: 'a',
-    tags: ['a','b','c'],
-    showTages(){
-        this.tags.forEach(function(tag){
-            console.log(this.title,tag);
-        },this);
-    }
-};
-video.showTages();
+const items = numbers.map((n,index) => {
+    return { index :   index,
+            id : n , 
+             name : 'apple',
+             price : 50,
+             color : 'red'
+    } ;    
+});
+
+console.log( "Apple", items); 

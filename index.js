6d125bca605547745e911b5434210cf574a5bbeb
Taral Panchal <taral.facile@outlook.com>
2021-03-10 16:44:13 +0530

    Movie Ex-7

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const numbers = [1, 2, 3, 4];
+const movies = [
+    { title : 'a', year: 2018, rating : 4.5 },
+    { title : 'b', year: 2018, rating : 4.7 }, 
+    { title : 'c', year: 2018, rating : 3 },
+    { title : 'd', year: 201, rating : 3.5 },
+];
 
-const max = getMax([]);
-console.log(max);
 
-function getMax(array) {
-    if (array.length === 0) return undefined;
-    return array.reduce((a,b) => (a > b) ? a : b);
-}  
+
+const titles =  movies
+    .filter(m => m.year === 2018 && m.rating >=4)
+    .short((a,b) => a.rating - b.rating)
+    .reverse()
+    .map(m => m.title)
+
+console.log(titles);

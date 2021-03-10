2462bab459497ec578e9fcc6cf618a891e5b5769
Taral Panchal <taral.facile@outlook.com>
2021-03-10 16:17:53 +0530

    get max Ex-6

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const number = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4];
 
-const count = countOccurrenece(number, 1);
-console.log(count);
+const max = getMax([]);
+console.log(max);
 
-function countOccurrenece(array, serachElement) {
-    return array.reduce((accumulator, current) => {
-    const occurrenece = (current === serachElement) ? 1 : 0; 
-        console.log(accumulator, current, serachElement);
-        return accumulator + occurrenece;
-    }, 0);
-}
+function getMax(array) {
+    if (array.length === 0) return undefined;
+    return array.reduce((a,b) => (a > b) ? a : b);
+}  

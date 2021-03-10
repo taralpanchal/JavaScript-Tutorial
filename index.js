ddaae14a118f5f96bbf26d16eb28cb18314ce9d9
Taral Panchal <taral.facile@outlook.com>
2021-03-10 17:49:43 +0530

    Exception Ex-3

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const circle = {
-    radius: 1,
-    get area() {
-        return Math.PI * this.radius * this.radius ;
-    }
- };
-
- console.log(circle.area);
+try {
+    const number = [1, 2, 3, 4];
+    const count = countOccurrenece(0, 1);
+    console.log(count);
+}
+catch (e){
+    console.log(e.message);
+}
+function countOccurrenece(array, serachElement) {
+    if(!Array.isArray(array))
+        throw new Error('Invalid array');
+    return array.reduce((accumulator, current) => {
+        const occurrenece = (current === serachElement) ? 1 : 0; 
+        console.log(accumulator, current, serachElement);
+        return accumulator + occurrenece;
+    }, 0);
+}

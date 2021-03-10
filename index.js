0fa03861c8d50888cb8172ad153e5069122ec2e4
Taral Panchal <taral.facile@outlook.com>
2021-03-10 17:41:43 +0530

    area of circle Ex-2 fun

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
- function sum(...items){
-    if (items.length === 1 && Array.isArray(items[0]))
-        items = [...items[0]];
-    return items.reduce((a,b) => a + b); 
-    // let total = 0;
-    //  for(let n of items)
-    //     total += n;
-    //  return total;
- }
- console.log(sum(1,2,3,4,5));
+const circle = {
+    radius: 1,
+    get area() {
+        return Math.PI * this.radius * this.radius ;
+    }
+ };
+
+ console.log(circle.area);
